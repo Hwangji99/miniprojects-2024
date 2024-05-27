dc66583cc74cd5ecdb34328c87bdfb16b925de95
Hwangji99 <ghkdwl1303@naver.com>
2024-05-27 16:32:31 +0900

    05-27 마무리

--- part2/win/SmartHomeMonitor/Views/DataBaseControl.xaml.cs
+++ part2/win/SmartHomeMonitor/Views/DataBaseControl.xaml.cs
@@ -1,8 +1,11 @@
-﻿using MQTTnet;
+﻿using MahApps.Metro.Controls;
+using MQTTnet;
 using MQTTnet.Client;
+using Newtonsoft.Json;
 using SmartHomeMonitoringApp.Logics;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -54,10 +57,10 @@ namespace SmartHomeMonitoringApp.Views
 
         private async void ConnectSystem()
         {
-            if (IsConnected == false)
+            if (IsConnected == false) // 연결이 안됐으면 처리
             {
                 // 한번도 접속을 안했으면 모두 연결
-                var mqttFactory = new MqttFactory();
+                var mqttFactory = new MqttFactory();    // 디자인 패턴 중 팩토리 패턴
                 Commons.MQTT_CLIENT = mqttFactory.CreateMqttClient();
                 // MQTT 브로커 아이피 연결속성
                 var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer(TxtBrokerUrl.Text).Build();
@@ -77,14 +80,92 @@ namespace SmartHomeMonitoringApp.Views
                 BtnConnect.IsChecked = true;
                 BtnConnect.Content = "MQTT 연결중";
             }
+            else
+            {
+                // 연결 후 연결 끊기
+                if(Commons.MQTT_CLIENT.IsConnected)
+                {
+                    Commons.MQTT_CLIENT.ApplicationMessageReceivedAsync -= MQTT_CLIENT_ApplicationMessageReceivedAsync;
+                    await Commons.MQTT_CLIENT.DisconnectAsync();
+
+                    IsConnected = false;
+                    BtnConnect.IsChecked = false;
+                    BtnConnect.Content = "Connect";
+                }
+            }
         }
 
         private Task MQTT_CLIENT_ApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs arg)
         {
             var payload = Encoding.UTF8.GetString(arg.ApplicationMessage.Payload);
             Debug.WriteLine(payload);
+            UpdateLog(payload); // TextBox에 추가
+            InsertData(payload); // DB에 저장
 
             return Task.CompletedTask; // Async에서 Task값을 넘겨줌
+        }
+
+        private void InsertData(string payload)
+        {
+            this.Invoke(() =>
+            {
+
+                var currValue = JsonConvert.DeserializeObject<Dictionary<string, string>>(payload);
+
+                //Debug.WriteLine("InsertData : " + currValue["CURR_DT"]);
+                //currValue["Dev_ID"], currValue["TYPE"], currValue["VALUE"];
+                if (currValue != null)
+                {
+                    try
+                    {
+                        using (SqlConnection conn = new SqlConnection(TxtConnString.Text))
+                        {
+                            conn.Open();
+                            var InsQuery = @"INSERT INTO [dbo].[SmartHomeData]
+                                                               ([DEV_ID]
+                                                               ,[CURR_DT]
+                                                               ,[TEMP]
+                                                               ,[HUMID])
+                                                         VALUES
+                                                               (@DEV_ID
+                                                               ,@CURR_DT
+                                                               ,@TEMP
+                                                               ,@HUMID)";
+
+                            SqlCommand cmd = new SqlCommand(InsQuery, conn);
+                            cmd.Parameters.AddWithValue("@DeV_ID", currValue["DEV_ID"]);
+                            cmd.Parameters.AddWithValue("@CURR_DT", currValue["CURR_DT"]); // string --> DateTime 자동 변환됨
+                            // cmd.Parameters.AddWithValue("@TYPE", currValue["TYPE"]);
+                            var splitValue = currValue["VALUE"].Split('|'); // SplitValue[0] = 온도, splitValue[1] = 습도
+                            cmd.Parameters.AddWithValue("@TEMP", splitValue[0]);
+                            cmd.Parameters.AddWithValue("@HUMID", splitValue[1]);
+
+                            if (cmd.ExecuteNonQuery() == 1)
+                            {
+                                UpdateLog(">>> DB Insert succeed.");
+                            }
+                            else
+                            {
+                                UpdateLog(">>> DB Insert failed");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        UpdateLog($"DB 에러발생 : {ex.Message}");
+                    }
+                }
+
+            });
+        }
+
+        private void UpdateLog(string payload)
+        {
+            this.Invoke(() =>
+            {
+                TxtLog.Text += $"{payload}\n";
+                TxtLog.ScrollToEnd(); // 스크롤이 생기기 시작하면 제일 아래로 포커스를 맞춰줌
+            });
         }
     }
 }
