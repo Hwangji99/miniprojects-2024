2b28bdf953d3042d715a57a752b6db289f8dc8c9
Hwangji99 <ghkdwl1303@naver.com>
2024-05-28 14:23:33 +0900

    05-28 마무리

--- part2/win/SmartHomeMonitor/Views/RealTimeControl.xaml.cs
+++ part2/win/SmartHomeMonitor/Views/RealTimeControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using LiveChartsCore.SkiaSharpView.Extensions;
+﻿using LiveChartsCore.SkiaSharpView.Drawing;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.VisualElements;
 using MahApps.Metro.Controls;
 using MQTTnet;
 using MQTTnet.Client;
@@ -13,6 +16,9 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
 
 namespace SmartHomeMonitoringApp.Views
 {
@@ -38,11 +44,12 @@ namespace SmartHomeMonitoringApp.Views
 
         private async void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
+            // MQTT 초기화 설정
             if (Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
             {   // 이미 다른 화면에서 MQTT를 연결한 경우
                 Commons.MQTT_CLIENT.ApplicationMessageReceivedAsync += MQTT_CLIENT_ApplicationMessageReceivedAsync;
             }
-            else
+            else // 한번도 연결한 적이 없으면
             {
                 var mqttFactory = new MqttFactory();
                 Commons.MQTT_CLIENT = mqttFactory.CreateMqttClient();
@@ -68,8 +75,23 @@ namespace SmartHomeMonitoringApp.Views
             return Task.CompletedTask; // Async에서 Task값을 넘겨주려면 이렇게
         }
 
+        // 앵귤러 차트를 위한 변수, 속성 선언
+        public IEnumerable<ISeries> HumidSeries { get; set; }
+
+        public IEnumerable<VisualElement<SkiaSharpDrawingContext>> VisualElements { get; set; } // 차트 만들어줌
+
+        public NeedleVisual Needle { get; set; }    // 바늘모양을 만들어줌
+
+        private static void SetStyle(
+        double sectionsOuter, double sectionsWidth, PieSeries<ObservableValue> series)
+        {
+            series.OuterRadiusOffset = sectionsOuter;
+            series.MaxRadialColumnWidth = sectionsWidth;
+        }
+
         private void UpdateChart(string payload)
         {
+            // 차트에 값 대입해서 차트가 나오도록
             this.Invoke(() =>
             {
                 var currValue = JsonConvert.DeserializeObject<Dictionary<string, string>>(payload);
@@ -86,8 +108,57 @@ namespace SmartHomeMonitoringApp.Views
                         series.DataLabelsSize = 40;
                     }
                 ));
-                ChtDiningTemp.Series = tempVal;
+                ChtLivingTemp.Series = ChtDiningTemp.Series = ChtBedTemp.Series = ChtBathTemp.Series = tempVal; // 차트 네개 다 같은 값을 할당
+
+
+                var sectionsOuter = 130;
+                var sectionsWidth = 20;
+
+                // 습도차트 값 할당
+                HumidSeries = GaugeGenerator.BuildAngularGaugeSections(
+                    new GaugeItem(humid,
+                    s => SetStyle(sectionsOuter, sectionsWidth, s))
+                );
+                ChtLivingHumid.Series = ChtDiningHumid.Series = ChtBedHumid.Series = ChtBathHumid.Series = HumidSeries;
+
+                // 습도를 나타낼 앵귤러차트를 초기화
+                // 앵귤러 차트를 그리기 위한 속성들 51 ~ 62행
+                Needle = new NeedleVisual { Value = humid };   // 차트 바늘 45로 초기화
+                VisualElements = new VisualElement<SkiaSharpDrawingContext>[]
+                {
+                new AngularTicksVisual
+                {   // 앵귤러 차트를 꾸미는 속성
+                    LabelsSize = 12,
+                    LabelsOuterOffset = 15,
+                    OuterOffset = 65,
+                    TicksLength = 15    // 차트 실선 길이
+                },
+                Needle
+                };
+
+                // 위에서 만든 화면 디자인을 차트에 적용
+                ChtLivingHumid.VisualElements = ChtDiningHumid.VisualElements = VisualElements;
+                ChtBedHumid.VisualElements = ChtBathHumid.VisualElements = VisualElements;
             });
+        }
+
+        private void BtnWarning_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'control': 'warning'}");
+        }
+
+        private void BtnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'control': 'normal'}");
+
+
+        }
+
+        private void BtnOff_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'control': 'off'}");
+
+
         }
     }
 }
